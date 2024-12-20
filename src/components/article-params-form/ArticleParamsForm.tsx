449437cdc4580febc406d038c1eb860f6d99ace9
ModestRaf <tanya.stargate@yandex.ru>
2024-12-20 23:17:21 +0300

    верстка готова (наверное)

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,15 +4,21 @@ import {Button} from 'src/ui/button';
 import {Text} from 'src/ui/text';
 import {
 	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
 	defaultArticleState,
-	ArticleStateType,
+	ArticleStateType, OptionType, contentWidthArr, backgroundColors,
 } from 'src/constants/articleProps';
-
+import {Select} from 'src/ui/select/Select'; // Импортируем компонент Select
 import styles from './ArticleParamsForm.module.scss';
+import {RadioGroup} from 'src/ui/radio-group';
+import {Separator} from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
-	// Все Hooks вызываются на верхнем уровне
+	// Состояние для открытия/закрытия сайдбара
 	const [isOpen, setIsOpen] = useState(false);
+
+	// Состояние для настроек статьи
 	const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
 
 	// Обработчик клика на кнопку "Стрелка"
@@ -21,13 +27,10 @@ export const ArticleParamsForm = () => {
 	};
 
 	// Обработчик изменения настроек
-	const handleChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
-		const {name, value} = e.target;
-
-		// Обновление состояния в зависимости от выбранного поля
+	const handleChange = (option: OptionType, name: keyof ArticleStateType) => {
 		setArticleState((prevState) => ({
 			...prevState,
-			[name]: value,
+			[name]: option,
 		}));
 	};
 
@@ -48,24 +51,46 @@ export const ArticleParamsForm = () => {
 			<ArrowButton isOpen={isOpen} onClick={handleToggleSidebar}/>
 			<aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>
 				<form className={styles.form}>
-					<Text as='h2' size={31} weight={800} uppercase={true}>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							gap: '50px',
+						}}
+					>
+						<Text as='h2' size={31} weight={800} uppercase={true}>
 						Задайте параметры
 					</Text>
-					<div>
-						<label>
-							Шрифт:
-							<select
-								name='fontFamilyOption'
-								value={articleState.fontFamilyOption.value}
-								onChange={handleChange}
-							>
-								{fontFamilyOptions.map((option) => (
-									<option key={option.value} value={option.value}>
-										{option.title}
-									</option>
-								))}
-							</select>
-						</label>
+						<Select
+							selected={articleState.fontFamilyOption}
+							options={fontFamilyOptions}
+							title='Шрифт'
+							onChange={(option) => handleChange(option, 'fontFamilyOption')}
+							placeholder='Выберите шрифт'
+						/>
+						<RadioGroup
+							name='Размер шрифта'
+							title='Размер шрифта'
+							options={fontSizeOptions}
+							selected={articleState.fontSizeOption}
+						/>
+						<Select
+							selected={articleState.fontColor}
+							options={fontColors}
+							title='Цвет шрифта'
+						/>
+						<Separator/>
+						<Select
+							selected={articleState.backgroundColor}
+							options={backgroundColors}
+							title='Цвет фона'
+						/>
+						<Select
+							selected={articleState.contentWidth}
+							options={contentWidthArr}
+							title='Ширина контента'
+						/>
+
 					</div>
 					<div className={styles.bottomContainer}>
 						<Button
